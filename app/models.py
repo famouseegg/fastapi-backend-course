8c193e98ff731e118b89bf6040adafe626122ee9
swient <B11217048@yuntech.edu.tw>
2024-12-12 16:04:45 +0800

    Add User model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,10 @@ class Todo(Base):
     description = Column(String,nullable=True)
     completed = Column(Boolean,default=False)
     due_data = Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)  # 自動生成的主鍵
+    username = Column(String(80), unique=True, nullable=False)
+    password_hash = Column(String(120), nullable=False)
+    profile_image = Column(String(120), nullable=True)  # 新增一個欄位來存儲圖像文件的路徑
